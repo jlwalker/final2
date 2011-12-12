3cb2ea91642b6175bccca2f305a485dcb0f3b060
Jonathan Walker <jonnie.l.walker@gmail.com>
2011-12-11 23:52:00 -0600

    initial commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,6 @@
+module ApplicationHelper
+  
+  def logo
+    image_tag("logo.png", :alt => "Jonnies App", :class => "round")
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Finalproject::Application.config.secret_token = 'e8ef581c13654ae7b3e9736e7c7251c2122db568f7f74f469a863be77977e192e81e0c30c03f339bd5b2a6b7d496456a163a2de6da2e109286e5456d062ecd63'
