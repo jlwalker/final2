ebdf5918dd6baf5e70e73e49807e8ce87ae058a9
Jonathan Walker <jonnie.l.walker@gmail.com>
2011-12-12 09:54:47 -0600

    just incase

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Finalproject::Application.routes.draw do
   match '/signup', :to => 'users#new'
   match '/signin', :to => 'sessions#new'
   match '/signout', :to => 'sessions#destroy'
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
